c5f5914a00e5404d41759f9f5970c234d3bcd517
Shreykhanna <khannashrey07@gmail.com>
2025-12-29 15:22:44 +1000

    Add frontend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,6 +7,7 @@ const app = express();
 const port = 3000;
 app.use(cors());
 app.use(express.json());
+
 app.post("/summarise-email", async (req, res) => {
   const { message } = req.body;
   const response = await summariseEmailAgent.invoke({
